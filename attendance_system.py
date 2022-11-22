e7719484187f97f7471e9e04bedd66f08f35185d
palashbhusari <palashshankarbhusari@gmail.com>
2022-11-21 21:16:34 -0800

    adding face recognition code with 2 modes

---
+++ attendance_system.py
@@ -0,0 +1,169 @@
+import face_recognition
+import cv2
+from datetime import datetime, timedelta
+import numpy as np
+import platform
+import pickle
+
+
+# Our list of known face encodings and a matching list of metadata about each face.
+known_face_encodings = []
+known_face_metadata = []
+mode = ''
+
+def save_known_faces():
+    with open("known_faces.dat", "wb") as face_data_file:
+        face_data = [known_face_encodings, known_face_metadata]
+        pickle.dump(face_data, face_data_file)
+        print("Known faces backed up to disk.")
+
+
+def load_known_faces():
+    global known_face_encodings, known_face_metadata
+
+    try:
+        with open("known_faces.dat", "rb") as face_data_file:
+            known_face_encodings, known_face_metadata = pickle.load(face_data_file)
+            print("Known faces loaded from disk.")
+    except FileNotFoundError as e:
+        print("No previous face data found - starting with a blank known face list.")
+        pass
+
+
+def lookup_known_face(face_encoding):
+    """
+    See if this is a face we already have in our face list
+    """
+    metadata = None
+
+    # If our known face list is empty, just return nothing since we can't possibly have seen this face.
+    if len(known_face_encodings) == 0:
+        return metadata
+
+    # Calculate the face distance between the unknown face and every face on in our known face list
+    # This will return a floating point number between 0.0 and 1.0 for each known face. The smaller the number,
+    # the more similar that face was to the unknown face.
+    face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+
+    # Get the known face that had the lowest distance (i.e. most similar) from the unknown face.
+    best_match_index = np.argmin(face_distances)
+
+    # If the face with the lowest distance had a distance under 0.6, we consider it a face match.
+    # 0.6 comes from how the face recognition model was trained. It was trained to make sure pictures
+    # of the same person always were less than 0.6 away from each other.
+    # Here, we are loosening the threshold a little bit to 0.65 because it is unlikely that two very similar
+    # people will come up to the door at the same time.
+    if face_distances[best_match_index] < 0.5:
+        # If we have a match, look up the metadata we've saved for it (like the first time we saw it, etc)
+        metadata = known_face_metadata[best_match_index]
+
+        # Update the metadata for the face so we can keep track of how recently we have seen this face.
+        ##metadata["time"] = datetime.now()
+    return metadata
+
+
+def register_new_face(face_encoding, face_image):
+    """
+    Add a new person to our list of known faces
+    """
+    # Add the face encoding to the list of known faces
+    known_face_encodings.append(face_encoding)
+    # Add a matching dictionary entry to our metadata list.
+    # We can use this to keep track of how many times a person has visited, when we last saw them, etc.
+
+    name = input("enter name for new face")
+    known_face_metadata.append({
+        "name": name,
+        "time": datetime.now(),
+        "face_image": face_image,
+    })
+    save_known_faces()
+    print("Saved new student")
+    exit()
+
+
+def main_loop():
+
+    video_capture = cv2.VideoCapture(0)
+
+    # Track how long since we last saved a copy of our known faces to disk as a backup.
+    number_of_faces_since_save = 0
+
+    while True:
+        # Grab a single frame of video
+        ret, frame = video_capture.read()
+
+        # Resize frame of video to 1/4 size for faster face recognition processing
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        rgb_small_frame = small_frame[:, :, ::-1]
+
+        # Find all the face locations and face encodings in the current frame of video
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+        # Loop through each detected face and see if it is one we have seen before
+        # If so, we'll give it a label that we'll draw on top of the video.
+        face_labels = []
+        for face_location, face_encoding in zip(face_locations, face_encodings):
+            # See if this face is in our list of known faces.
+            metadata = lookup_known_face(face_encoding)
+            
+            # if metadata is None:
+            #     print("new face")
+
+            # If we found the face, label the face with some useful information.
+            if metadata is not None:
+                face_label = metadata['name']
+
+            # # If this is a brand new face, add it to our list of known faces
+            else:
+                face_label = "Student not registered!"
+
+                # Grab the image of the the face from the current frame of video
+                top, right, bottom, left = face_location
+                face_image = small_frame[top:bottom, left:right]
+                face_image = cv2.resize(face_image, (150, 150))
+
+                # Add the new face to our known face data
+                if mode == '2':
+                    register_new_face(face_encoding, face_image)
+
+            face_labels.append(face_label)
+
+        # Draw a box around each face and label each face
+        for (top, right, bottom, left), face_label in zip(face_locations, face_labels):
+            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
+
+            # Draw a box around the face
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+            # Draw a label with a name below the face
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            cv2.putText(frame, face_label, (left + 6, bottom - 6), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 255, 255), 1)
+
+        # Display the final frame of video with boxes drawn around each detected fames
+        cv2.imshow('Video', frame)
+
+        # Hit 'q' on the keyboard to quit!
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            #save_known_faces()
+            break
+
+
+    # Release handle to the webcam
+    video_capture.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    mode = input("Select mode \n'1' Take Attendace \n'2' Register a new student \n")
+    if mode == '2':
+        start = input("Please place your head in center of the screen and press enter")
+    load_known_faces()
+    main_loop()
